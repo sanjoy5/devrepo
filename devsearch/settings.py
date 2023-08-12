1da22e07da814f3f1dcc252bff0500eefa3f97d7
Sanjoy Sarker <sarkerd52@gmail.com>
2023-08-12 20:16:11 +0600

    bug fixed

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -130,13 +130,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-MEDIA_URL = 'images/'
+STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 
-MEDIA_ROOT = BASE_DIR / 'static/images'
-STATIC_ROOT = BASE_DIR / 'staticfiles_build/static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/media')
 
 
 # EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
