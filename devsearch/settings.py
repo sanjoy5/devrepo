6218b2289d0d40e8e4c25b6047eca1807d7fb686
Sanjoy Sarker <sarkerd52@gmail.com>
2023-08-13 12:52:18 +0600

    bug fixed

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%@4huiaxp@3nflm$&%gjqw51km%mf*)k-e7587^twrv41mnbjh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh', '127.0.0.1', 'localhost']
 
 
 # Application definition
