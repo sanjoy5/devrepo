7f486f7810a276724916843980f17721fc4b5944
Sanjoy Sarker <sarkerd52@gmail.com>
2023-08-13 12:50:03 +0600

    bug fixed

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-%@4huiaxp@3nflm$&%gjqw51km%mf*)k-e7587^twrv41mnbjh
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.vercel.app', '.now.sh', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
